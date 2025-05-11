a19088c3b6c5962962e7551737b2052b31670e74
Ajay Gohil <ajaygohil2563@gmail.com>
2025-05-11 11:53:03 +0530

    feat: completed the update and delete api.

--- config/database.py
+++ config/database.py
@@ -1,6 +1,11 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
+DB_USER_NAME =  os.getenv("DB_USER_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-uri = "mongodb+srv://ajayginexture:ajay-admin@cluster0.s1xscly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{DB_USER_NAME}:{DB_PASSWORD}@cluster0.s1xscly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri)
 
--- routers/todo_router.py
+++ routers/todo_router.py
@@ -1,15 +1,14 @@
+from bson import ObjectId
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
-
 from config.database import collection_name
 from schemas.schemas import list_of_serials
 from pydantic_models.todo_model import Todo
 router = APIRouter()
 
-
 @router.post("/add-post")
 async def add_post(todo: Todo):
-    """ This method add post in db."""
+    """ This API add post in db."""
     collection_name.insert_one(dict(todo))
     return JSONResponse({
             "info":"inserted successfully"
@@ -19,6 +18,27 @@ async def add_post(todo: Todo):
 
 @router.get("/")
 async def get_all_post():
-    """ This method get all posts. """
+    """ This API get all posts. """
     todos =  list_of_serials(collection_name.find())
     return todos
+
+@router.put("/{id}")
+async def update_post(id: str, todo: Todo):
+    """ This API update post in DB."""
+    collection_name.find_one_and_update({"_id":ObjectId(id)}, {"$set":dict(todo)})
+    return JSONResponse({
+        "info": "updated successfully"
+    },
+        status_code=200
+    )
+
+@router.delete("/{id}")
+async def delete_post(id: str):
+    """ This API delete the post in the DB. """
+    collection_name.find_one_and_delete({"_id": ObjectId(id)})
+    return JSONResponse({
+        "info": "Deleted successfully"
+    },
+        status_code=200
+    )
+
